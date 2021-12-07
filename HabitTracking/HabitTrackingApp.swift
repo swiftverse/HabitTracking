4dd1005b9feee0b7db3b2b0b56629a5b1461b3a3
shivgirja <amitsrivastava115@yahoo.com>
2021-12-07 15:02:50 +0530

    Initial Commit

---
+++ HabitTracking/HabitTrackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackingApp.swift
+//  HabitTracking
+//
+//  Created by Amit Shrivastava on 07/12/21.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
